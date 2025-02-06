e33bead646c056566eecbad392f568c521914f0e
Shruti Sharma <106474632+okshrutii@users.noreply.github.com>
2025-02-07 00:49:20 +0530

    Update Practical 2.py

--- Practical 2.py
+++ Practical 2.py
@@ -1,17 +1,17 @@
 #PRACTICAL 2
 #AIM: A program that calculates the area and perimeter of a rectangle.
 while True:
-1-float (input("Enter the length: "))
-b= float(input("\nEnter the breadth: "))
-print("\nl.Area\n2.Perimeter\n3.Exit")
-cho-int (input("\nEnter the choice: "))
-if cho 1:
-area 1
-print("\nArea of the rectangle with length", end=" ")
-print (1, "and breadth", b, "is: ",area)
+  1=float (input("Enter the length: "))
+  b=float(input("\nEnter the breadth: "))
+  print("\nl.Area\n2.Perimeter\n3.Exit")
+  cho+int (input("\nEnter the choice: "))
+if cho==1:
+  area=1
+  print("\nArea of the rectangle with length", end=" ")
+  print (1, "and breadth", b, "is: ",area)
 elif cho 2:
-per 2*(1+b)
+  per 2*(1+b)
 elif cho--3:
-break
+  break
 else:
-print("Invalid Input!")
+  print("Invalid Input!")
