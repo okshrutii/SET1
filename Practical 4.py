4a635c08927a9ce585e42fef314a9e9f51609dfb
Shruti Sharma <106474632+okshrutii@users.noreply.github.com>
2025-02-07 00:40:21 +0530

    Create Practical 4.py

---
+++ Practical 4.py
@@ -0,0 +1,13 @@
+#PRACTICAL 4
+#AIM: A program that calculates the average of a list of numbers.
+while True:
+print("1. Calculate the Average of the list.\n2.Exit\n")
+cho int (input ("Enter your choice: "))
+if cho == 1:
+lis eval(input("\nEnter list of elements: "))
+average sum (lis)/len (lis)
+print("\nAverage of the list", average)
+elif cho==2:
+break
+else:
+print("\nInvalid Input!")
