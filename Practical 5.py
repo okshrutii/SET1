a51b209020bf938f096b66ef3b20880790c9d063
Shruti Sharma <106474632+okshrutii@users.noreply.github.com>
2025-02-07 00:47:07 +0530

    Update Practical 5.py

--- Practical 5.py
+++ Practical 5.py
@@ -2,13 +2,13 @@
 #AIM: A program that checks if a given year is a leap year.
 def is leap year (year):
 if (year % 40 and year % 100 != 0) or (year % 400 == 0):
-return True
+  return True
 else:
-return False
-input year int (input("Enter a year: "))
-if input year <= 0:
-raise ValueError ("Please enter a valid positive year.")
+  return False
+  input_year=int (input("Enter a year: "))
+if input_year <= 0:
+  raise ValueError ("Please enter a valid positive year.")
 if is_leap_year(input_year):
-print("{) is a leap year.".format(input_year))
+  print("{) is a leap year.".format(input_year))
 else:
-print("() is not a leap year.".format(input_year))
+  print("() is not a leap year.".format(input_year))
